e7084c1c9b812f7450b0bc71c1aed8858bf12843
alirez khayatpour <alireza_khayatpour@yahoo.com>
2023-11-10 16:57:00 +0330

    add .table-responsive and edit & delete button to AddBook.jsx in BookList Project

--- src/project/bookListApp/AddBook.jsx
+++ src/project/bookListApp/AddBook.jsx
@@ -37,7 +37,7 @@ class AddBook extends Component {
         }
     }
     render() {
-
+        const { books, name, author, year } = this.state
         return (
             <div className='AddBook mt-3'>
                 <Helmet>
@@ -45,102 +45,106 @@ class AddBook extends Component {
                 </Helmet>
                 <Header2 />
                 <div className='container position-relative'>
-                    <div className='text-end position-absolute top-0 end-0'>
-                        <Link to="/" className='btn btn-warning'>خانه</Link>
-                    </div>
-                </div>
-                <div className='d-flex flex-column align-items-center'>
-                    <div className='card myCard  w-75 h-auto'>
-                        <div className="card-body">
-                            <form onSubmit={(event) => this.onSubmitHandler(event)}>
-                                <div className="container">
-                                    <div className="row justify-content-center" style={{ fontSize: 13, fontWeight: "bold", color: "whitesmoke" }}>
-                                        <div className="col-12 col-md-10 " >
-                                            <div className="row  align-items-center mt-1 mt-md-3 justify-content-center">
-                                                <div className="d-none d-lg-block col-2">
-                                                    <label htmlFor="input1" className="col-form-label">نام کتاب :</label>
+
+                    <div className='d-flex flex-column align-items-center'>
+                        <div className='card myCard  w-75 h-auto'>
+                            <div className="card-body">
+                                <form onSubmit={(event) => this.onSubmitHandler(event)}>
+                                    <div className="container">
+                                        <div className="row justify-content-center" style={{ fontSize: 13, fontWeight: "bold", color: "whitesmoke" }}>
+                                            <div className="col-12 col-md-10 " >
+                                                <div className="row  align-items-center mt-1 mt-md-3 justify-content-center">
+                                                    <div className="d-none d-lg-block col-2">
+                                                        <label htmlFor="input1" className="col-form-label">نام کتاب :</label>
+                                                    </div>
+                                                    <div className="col-md-9 col-lg-8">
+                                                        <input type="text"
+                                                            id="input1"
+                                                            className="form-control"
+                                                            placeholder='نام کتاب'
+                                                            name="name"
+                                                            value={name}
+                                                            onChange={(event) => this.onChangeHandler(event)}
+                                                        />
+                                                    </div>
                                                 </div>
-                                                <div className="col-md-9 col-lg-8">
-                                                    <input type="text"
-                                                        id="input1"
-                                                        className="form-control"
-                                                        placeholder='نام کتاب'
-                                                        name="name"
-                                                        value={this.state.name}
-                                                        onChange={(event) => this.onChangeHandler(event)}
-                                                    />
+                                                <div className="row  align-items-center justify-content-center mt-1 mt-md-3">
+                                                    <div className="d-none d-lg-block col-2">
+                                                        <label htmlFor="input2" className="col-form-label"> نویسنده  :</label>
+                                                    </div>
+                                                    <div className="col-md-9 col-lg-8">
+                                                        <input type="text"
+                                                            id="input2"
+                                                            className="form-control"
+                                                            placeholder='نویسنده'
+                                                            name="author"
+                                                            value={author}
+                                                            onChange={(event) => this.onChangeHandler(event)}
+                                                        />
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div className="row  align-items-center justify-content-center mt-1 mt-md-3">
-                                                <div className="d-none d-lg-block col-2">
-                                                    <label htmlFor="input2" className="col-form-label"> نویسنده  :</label>
-                                                </div>
-                                                <div className="col-md-9 col-lg-8">
-                                                    <input type="text"
-                                                        id="input2"
-                                                        className="form-control"
-                                                        placeholder='نویسنده'
-                                                        name="author"
-                                                        value={this.state.author}
-                                                        onChange={(event) => this.onChangeHandler(event)}
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="row align-items-center justify-content-center mt-1 mt-md-3">
-                                                <div className="d-none d-lg-block col-2">
-                                                    <label htmlFor="input3" className="col-form-label"> سال تالیف :</label>
-                                                </div>
-                                                <div className="col-md-9 col-lg-8">
-                                                    <input type="text"
-                                                        id="input3"
-                                                        className="form-control"
-                                                        placeholder='سال تالیف'
-                                                        name="year"
-                                                        value={this.state.year}
-                                                        onChange={(event) => this.onChangeHandler(event)}
-                                                    />
+                                                <div className="row align-items-center justify-content-center mt-1 mt-md-3">
+                                                    <div className="d-none d-lg-block col-2">
+                                                        <label htmlFor="input3" className="col-form-label"> سال تالیف :</label>
+                                                    </div>
+                                                    <div className="col-md-9 col-lg-8">
+                                                        <input type="text"
+                                                            id="input3"
+                                                            className="form-control"
+                                                            placeholder='سال تالیف'
+                                                            name="year"
+                                                            value={year}
+                                                            onChange={(event) => this.onChangeHandler(event)}
+                                                        />
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div className="col-9 d-grid text-center mt-4">
-                                            <button className="btn btn-warning">افزودن کتاب</button>
+                                            <div className="col-9 d-grid text-center mt-4">
+                                                <button className="btn btn-warning">افزودن کتاب</button>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
 
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
+
+                    <section id='bookList' className='overflow-scroll'>
+                        <div className='d-flex flex-column align-items-center mt-3'>
+                            {
+                                books.length > 0 &&
+                                <div className='card  w-75 h-auto text-center'>
+                                    <div className='table-responsive w-100'>
+                                            <table className='table table-striped  align-middle'>
+                                            <thead>
+                                                <tr>
+                                                    <th className='fw-bold'>ردیف</th>
+                                                    <td className='fw-bold'>نام کتاب</td>
+                                                    <td className='fw-bold'>نویسنده</td>
+                                                    <td className='fw-bold'>سال تالیف</td>
+                                                    <td className='fw-bold'>عملیات</td>
+                                                </tr>
+                                            </thead>
+                                            <tbody className='overflow-scroll'>
+                                                {
+                                                    books.length > 0 &&
+                                                    books.map((book) => (
+                                                        <Book key={book.id} {...book} />
+                                                    ))
+                                                }
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            }
+                        </div>
+
+                    </section>
+
                 </div>
-                <div className='d-flex flex-column align-items-center mt-3'>
-                    <div className='card w-75 h-auto text-center'>
-                    <div className="table-responsive">
-                            <table className='table table-striped'>
-                                <thead>
-                                    <tr>
-                                        <th>ردیف</th>
-                                        <td className='fw-bold'>نام کتاب</td>
-                                        <td className='fw-bold'>نویسنده</td>
-                                        <td className='fw-bold'>سال تالیف</td>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
 
-                                        this.state.books.map((book) => (
-                                            <Book key={book.id} {...book} />
-                                        ))
-                                    }
-                                </tbody>
-                            </table>
-
-                    </div>
-                    </div>
-                </div>
-
-
-
-            </div >
+            </div>
         );
     }
 }
--- src/project/bookListApp/Book.jsx
+++ src/project/bookListApp/Book.jsx
@@ -9,13 +9,16 @@ class Book extends Component {
             <>
 
                 <tr>
-                    <th>{id}</th>
-                    <td>{name}</td>
-                    <td>{author}</td>
-                    <td>{year}</td>
-                    <td>
-                        <div className='btn btn-sm btn-success'>ویرایش</div>
-                        <div className='btn btn-sm btn-danger'>حذف</div>
+                    <th style={{ fontSize: "1rem"}}>{id}</th>
+                    <td style={{ fontSize: "1rem" }}>{name}</td>
+                    <td style={{ fontSize: "1rem" }}>{author}</td>
+                    <td style={{ fontSize: "1rem" }}>{year}</td>
+                    <td style={{ fontSize: "1rem" }}>
+                    <div className="d-flex justify-content-center flex-nowrap">
+                            <div className='btn btn-sm btn-success me-1'>ویرایش</div>
+                            <div className='btn btn-sm btn-danger'>حذف</div>
+
+                    </div>
                     </td>
                 </tr>
 
