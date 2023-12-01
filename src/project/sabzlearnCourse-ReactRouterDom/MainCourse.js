28eb46ad656a5dd8c17822885e3f6eec3324d401
alirez khayatpour <alireza_khayatpour@yahoo.com>
2023-12-01 15:53:54 +0330

    courses Project: change somthine in MainCourse.jsx by useLocation

--- src/project/sabzlearnCourse-ReactRouterDom/MainCourse.js
+++ src/project/sabzlearnCourse-ReactRouterDom/MainCourse.js
@@ -1,10 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+
 import Header from './Header'
+import "./MainCourse.css"
+
 const MainCourse = () => {
+    const {pathname} = useLocation()
+
     return (
         <>
             <Header />
-            <Outlet />
+            {
+                pathname === "/rrd" ?
+                <p p className='header-text text-center text-white'>مینی پروژه دوره های آموزشی</p>
+                    : <Outlet />
+            }
         </>
     );
 }
