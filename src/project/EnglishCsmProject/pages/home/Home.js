f55800ea83cd7c9ed448f7bd6c09bd9022add990
alirez khayatpour <alireza_khayatpour@yahoo.com>
2023-12-27 13:05:31 +0330

    Cms Project: use from useMediaQuery in MUI for hidden or show elements home.js to respansive

--- src/project/EnglishCsmProject/pages/home/Home.js
+++ src/project/EnglishCsmProject/pages/home/Home.js
@@ -9,14 +9,14 @@ import { Button } from '@mui/material';
 import { Modal } from 'react-bootstrap';
 
 export default function Home() {
-  let matches = useMediaQuery('(min-width:768px)') //MUI
+  let matches = useMediaQuery('(min-width:768px)') //MUI Hook
   const [features] = useState(
     [
-      { title: "Average", variant: "Light",   textColor: "dark", arrow: "down", telotance: "-10.4" },
-      { title: "Sales",   variant: "warning", textColor: "dark", arrow: "up",   telotance: "+5.3" },
-      { title: "Cost",    variant: "info",    textColor: "dark", arrow: "down", telotance: "-3.2" },
+      { title: "Average", variant: "Light", textColor: "dark", arrow: "down", telotance: "-10.4" },
+      { title: "Sales", variant: "warning", textColor: "dark", arrow: "up", telotance: "+5.3" },
+      { title: "Cost", variant: "info", textColor: "dark", arrow: "down", telotance: "-3.2" },
     ])
-  const [showMemberModal, setShowMemberModal]   = useState(false)
+  const [showMemberModal, setShowMemberModal] = useState(false)
   const [showTransacModal, setShowTransacModal] = useState(false)
 
   return (
@@ -25,7 +25,7 @@ export default function Home() {
         <div className="card m-3 p-3" style={{ background: "rgba(255,255,255,.7)" }}>
           <div className="container">
             <div className="row justify-content-evenly align-items-center mt-3">
-            
+
               {
                 matches ? (
                   features.map((feature) => (
@@ -33,14 +33,14 @@ export default function Home() {
                       <Features key={feature.title} {...feature} />
                     </div>
                   ))
-                ):(
-                    <div className='col-12'>
-                      <HomeModal features={features} />
-                    </div>
+                ) : (
+                  <div className='col-12'>
+                    <HomeModal features={features} />
+                  </div>
                 )
-               
+
               }
-             
+
             </div>
           </div>
         </div>
@@ -62,47 +62,61 @@ export default function Home() {
 
       </section>
 
-      <section id='newJoinMember'>
+      <section id='report-table'>
 
         <div className="container">
-          <div className="row  justify-content-between ">
-            {/* todo display Lg-Xl */}
-            <div className='col-12 col-md-6 mb-lg-3 d-none d-lg-block'>
-              <h3 className="widgetTitle" style={{ direction: "ltr" }}>Transaction Reports:</h3>
-              <div className="card m-2 p-3" style={{ background: "rgba(255,255,255,.9)" }}>
-                <LastTransaction />
-              </div>
-            </div>
-            <div className='col-12 col-md-6 d-none d-lg-block'>
-              <h3 className="widgetTitle" style={{ direction: "ltr" }}>Recently member :</h3>
-              <div className="card m-2 p-3" style={{ background: "rgba(255,255,255,.9)" }}>
-                <NewJoinMember />
-              </div>
-            </div>
+          <div className="row justify-content-between ">
+            {
+              matches ? (
+                <>
+                  {/* todo display Lg-Xl */}
+                  <div className='col-lg-6'>
+                    <h3 className="widgetTitle" style={{ direction: "ltr" }}>Transaction Reports:</h3>
+                    <div className="card m-2 p-3" style={{ background: "rgba(255,255,255,.9)" }}>
+                      <LastTransaction />
+                    </div>
+                  </div>
+                  <div className='col-lg-6'>
+                    <h3 className="widgetTitle" style={{ direction: "ltr" }}>Recently member :</h3>
+                    <div className="card m-2 p-3" style={{ background: "rgba(255,255,255,.9)" }}>
+                      <NewJoinMember />
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <>
+                  {/* todo display Xs-Sm-Md */}
+                  <div className='d-flex justify-content-center'>
+                    <Button
+                      variant='contained'
+                      color='secondary'
+                      className='me-2'
+                      onClick={() => setShowMemberModal((prev) => !prev)}
+                    >
+                      Recently Member
+                    </Button>
+                    <Button
+                      variant='contained'
+                      color='secondary'
+                      onClick={() => setShowTransacModal((prev) => !prev)}
+                    >
+                      Transaction Report
+                    </Button>
+                  </div>
+                </>
+              )
 
-            {/* todo display Xs-Sm-Md */}
-            {/* todo: Buttons */}
-              <div className='d-flex justify-content-center d-lg-none '>
-              <Button variant='contained'
-                color='secondary'
-                className='me-2'
-                onClick={() => setShowMemberModal((prev) => !prev)}
-              >
-                Recently Member
-              </Button>
-              <Button variant='contained'
-                color='secondary'
-                onClick={() => setShowTransacModal((prev) => !prev)}
-              >
-                Transaction Report
-              </Button>
-            </div>
+            }
+
+
+
+
+
             {/* todo: Modals */}
-            <div className='d-block d-lg-none'>
+            <div className='memberModal'>
               <Modal show={showMemberModal}
                 fullscreen="lg-down"
                 onHide={() => setShowMemberModal(false)}
-                className='d-block d-lg-none'
                 style={{ direction: "ltr" }}
               >
                 <Modal.Header className='bg-primary-subtle' closeButton>
@@ -113,8 +127,9 @@ export default function Home() {
                 </Modal.Body>
               </Modal>
             </div>
-            <div className='d-block d-lg-none'>
-              <Modal show={showTransacModal}
+            <div className='transactModal'>
+              <Modal
+                show={showTransacModal}
                 fullscreen="lg-down"
                 onHide={() => setShowTransacModal(false)}
                 className='d-block d-lg-none '
@@ -131,7 +146,6 @@ export default function Home() {
 
           </div>
         </div>
-
       </section>
     </>
   );
