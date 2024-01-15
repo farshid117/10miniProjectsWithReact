f2dca3206cc863cbb52dd9996e66a630c1e1492c
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-01-15 16:09:46 +0330

    Hooks Project: Emplementation Example2-part1 without useMemo

--- src/project/Hooks/useMemo/UseMemoEx2.jsx
+++ src/project/Hooks/useMemo/UseMemoEx2.jsx
@@ -1,11 +1,89 @@
 
-import React from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const UseMemoEx2 = () => {
+    const inputRef = useRef(null)
+    const [notes, setNotes] = useState([])
+    const [newNote, setNewNote] = useState("")
+
+    useEffect(() => {
+        inputRef.current.focus()
+    }, [])
+
+    const addNote = (event) => {
+        event.preventDefault()
+        if (newNote.trim().length > 0) {
+            const newNoteObj = {
+                id: notes.length + 1,
+                title: newNote
+            }
+            setNotes(preNotes => [...preNotes, newNoteObj])
+            setNewNote("")
+            inputRef.current.focus()
+
+        } else {
+            alert("please enter a note")
+        }
+    }
+
+    const resetNotesArray = () => {
+        setNotes([])
+        setNewNote("")
+        inputRef.current.focus()
+    }
+
     return (
-        <div>
-            useMemo Example2
-        </div>
+        <>
+            <div className="container">
+                <div className='row justify-content-center mt-5'>
+                    <div className="col-12 col-md-6">
+
+                        <form className='d-flex justify-content-center'>
+                            <button className='btn btn-primary btn-sm' onClick={addNote} >Add New Note</button>
+                            <input
+                                ref={inputRef}
+                                type='text'
+                                dir='ltr'
+                                className='form-control w-50 ms-3'
+                                value={newNote}
+                                onChange={e => setNewNote(e.target.value.trim())}
+                            />
+                        </form>
+
+                    </div>
+                </div>
+                <div className="row justify-content-center mt-3">
+                    <div className="col-12 col-md-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <ul dir='ltr' >
+                                    {notes.length > 0 ?
+                                        (notes.map(note => {
+                                            console.log("render map Black")
+                                            return (
+                                                <li key={note.id} className='fw-bold' >{note.id} - {note.title}</li>
+                                            )
+                                        }
+
+                                        )) : (
+                                            <p className='text-center fw-bold'>Notes Array is empty...
+                                                <br /> <span className='fw-normal'>pls enter new notes</span>
+                                            </p>
+                                        )
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-12 text-center">
+                        <button className='btn btn-warning btn-sm mt-3' onClick={resetNotesArray} >Reset Notes</button>
+                    </div>
+                </div>
+
+            </div>
+        </>
     );
 }
 
